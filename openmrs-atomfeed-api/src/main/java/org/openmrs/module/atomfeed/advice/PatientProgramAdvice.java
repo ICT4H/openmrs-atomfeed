07544b01661f36667cc4b8a9b995c4e0c6fa1cda
Preethi <preethi.ms34@gmail.com>
2016-08-05 15:50:50 +0530

    Preethi, Angshu | Creating transaction manager and event service on demand instead of constructor

--- openmrs-atomfeed-api/src/main/java/org/openmrs/module/atomfeed/advice/PatientProgramAdvice.java
+++ openmrs-atomfeed-api/src/main/java/org/openmrs/module/atomfeed/advice/PatientProgramAdvice.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.atomfeed.advice;
 
-import org.ict4h.atomfeed.server.repository.AllEventRecordsQueue;
 import org.ict4h.atomfeed.server.repository.jdbc.AllEventRecordsQueueJdbcImpl;
 import org.ict4h.atomfeed.server.service.Event;
 import org.ict4h.atomfeed.server.service.EventService;
@@ -26,13 +25,13 @@ public class PatientProgramAdvice implements AfterReturningAdvice {
     private static final String RAISE_PATIENT_PROGRAM_EVENT_GLOBAL_PROPERTY = "atomfeed.publish.eventsForPatientProgramStateChange";
     private static final String PATIENT_PROGRAM_EVENT_URL_PATTERN_GLOBAL_PROPERTY = "atomfeed.event.urlPatternForProgramStateChange";
     private static final String DEFAULT_PATIENT_PROGRAM_URL_PATTERN = "/openmrs/ws/rest/v1/programenrollment/{uuid}?v=full";
-    private final AtomFeedSpringTransactionManager atomFeedSpringTransactionManager;
-    private final EventService eventService;
+    private AtomFeedSpringTransactionManager atomFeedSpringTransactionManager;
+    private EventService eventService;
+    private final Object eventServiceMonitor = new Object();
+    private final Object txManagerMonitor = new Object();
 
     public PatientProgramAdvice() throws SQLException {
-        atomFeedSpringTransactionManager = new AtomFeedSpringTransactionManager(getSpringPlatformTransactionManager());
-        AllEventRecordsQueue allEventRecordsQueue = new AllEventRecordsQueueJdbcImpl(atomFeedSpringTransactionManager);
-        this.eventService = new EventServiceImpl(allEventRecordsQueue);
+
     }
 
     @Override
@@ -41,11 +40,11 @@ public class PatientProgramAdvice implements AfterReturningAdvice {
             String contents = getUrlPattern().replace("{uuid}",((PatientProgram) returnValue).getUuid());
             final Event event = new Event(UUID.randomUUID().toString(), TITLE, DateTime.now(), (URI) null, contents, CATEGORY);
 
-            atomFeedSpringTransactionManager.executeWithTransaction(
+            getAFTxManager().executeWithTransaction(
                     new AFTransactionWorkWithoutResult() {
                         @Override
                         protected void doInTransaction() {
-                            eventService.notify(event);
+                            getEventService().notify(event);
                         }
 
                         @Override
@@ -55,6 +54,28 @@ public class PatientProgramAdvice implements AfterReturningAdvice {
                     }
             );
         }
+    }
+
+    private EventService getEventService() {
+        if (eventService == null) {                // Single Checked
+            synchronized (eventServiceMonitor) {
+                if (eventService == null) {        // Double checked
+                    this.eventService = new EventServiceImpl(new AllEventRecordsQueueJdbcImpl(getAFTxManager()));
+                }
+            }
+        }
+        return this.eventService;
+    }
+
+    private AtomFeedSpringTransactionManager getAFTxManager() {
+        if (this.atomFeedSpringTransactionManager == null) {
+            synchronized (txManagerMonitor) {
+                if(this.atomFeedSpringTransactionManager == null) {
+                    this.atomFeedSpringTransactionManager = new AtomFeedSpringTransactionManager(getSpringPlatformTransactionManager());
+                }
+            }
+        }
+        return this.atomFeedSpringTransactionManager;
     }
 
     private boolean shouldRaiseRelationshipEvent() {
